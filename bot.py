7bb7e7c656466071b1a615303fd7b2eb1265c895
Boxdox <vaibhavkandwal@pm.me>
2019-09-12 01:15:23 +0530

    bugfix

--- bot.py
+++ bot.py
@@ -18,18 +18,21 @@ def start(update, context):
 
 def get_profile(update, context):
 	no_account="Sorry, this page isn&#39;t available"
-	username=update.message.text.split()[1]
-	url = "https://www.instagram.com/"+username
-
-	source  = requests.get(url).text
-	if source.find(no_account)==-1:
-		l = source.find("pic_url_hd")+13
-		r = source.find("requested_by_viewer")-3
-
-		img_url = source[l:r]
-		update.message.reply_photo(photo=img_url)
+	if len(update.message.text.split())!=2:
+		update.message.reply_text("Usage: /get username")
 	else:
-		update.message.reply_text("This account could not be found, try again.")
+		username=update.message.text.split()[-1]
+		url = "https://www.instagram.com/"+username
+
+		source  = requests.get(url).text
+		if source.find(no_account)==-1:
+			l = source.find("pic_url_hd")+13
+			r = source.find("requested_by_viewer")-3
+
+			img_url = source[l:r]
+			update.message.reply_photo(photo=img_url)
+		else:
+			update.message.reply_text("This account could not be found, try again.")
 
 def main():
 	updater = Updater(getenv('token'), use_context=True)
